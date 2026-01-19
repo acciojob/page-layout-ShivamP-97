164b9029ced799529535697da45673385dd78d76
= <shivampathak19972@gmail.com>
2026-01-19 23:32:04 +0530

    Test

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './../styles/App.css';
-import PageLayout from "./PageLayout"; // Make sure this path is correct
+import PageLayout from "./PageLayout";
 
 const App = () => {
   return (
@@ -8,9 +8,9 @@ const App = () => {
       {/* Do not remove the main div */}
       <PageLayout
         header={<h1>My Website Header</h1>}
-        footer={<p>© 2023 My Website</p>}
+        footer={<p>© 2023 My Website. All rights reserved.</p>}
       >
-        <p>Welcome to my website. Here is the main content!</p>
+        <p>Welcome to my website</p>
       </PageLayout>
     </div>
   );
--- src/components/PageLayout.js
+++ src/components/PageLayout.js
@@ -1,6 +1,5 @@
 import React from "react";
 
-// PageLayout component
 const PageLayout = ({ header, footer, children }) => {
   return (
     <div>
